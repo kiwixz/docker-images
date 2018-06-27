d5abc8e18b3b8a366111b60495609c620d10bf4b
kiwixz <kiwixz@outlook.com>
2018-06-27 03:43:37 +0200

    [arch-vcpkg] squash layers, cleanup vcpkg, disable metrics, preinstall cmake

--- arch-vcpkg/bootstrap.sh
+++ arch-vcpkg/bootstrap.sh
@@ -4,14 +4,21 @@ set -e
 
 pacman --needed --noconfirm -Sy  \
     curl unzip tar  `# "official" dependencies`  \
-    gcc  `# to compile vcpkg`  \
     git  `# to clone vcpkg`  \
+    gcc  `# to compile vcpkg`  \
     which  `# for vcpkg to find tools in path`  \
+    cmake  `# to install vcpkg libs`
 
 cd "/opt"
 git clone --depth=1 "https://github.com/Microsoft/vcpkg.git"
+
 cd "vcpkg"
-./bootstrap-vcpkg.sh
+./bootstrap-vcpkg.sh -disableMetrics
 ln -s "/opt/vcpkg/vcpkg" "/usr/bin/"
 vcpkg integrate install
 chmod -R a+rw "."
+
+find "toolsrc" -maxdepth 1 -name 'build*' -type d -exec rm -rf {} \;
+rm -rf "packages" "buildtrees"
+rm -rf downloads/tools/cmake-*
+find "downloads" -name '*' ! -path "downloads" ! -path 'downloads/tools*' -delete
