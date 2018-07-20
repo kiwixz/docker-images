2a8663da27a5461d77a51aafcca05e104ea5dcf6
kiwixz <kiwixz@outlook.com>
2018-07-20 21:47:51 +0200

    [arch] uninstall superfluous packages

--- arch/bootstrap.sh
+++ arch/bootstrap.sh
@@ -3,4 +3,8 @@
 set -e
 
 echo "Server=https://archive.archlinux.org/repos/$(date -d 'yesterday' +%Y/%m/%d)/\$repo/os/\$arch" > "/etc/pacman.d/mirrorlist"
-pacman --noconfirm -Syyuu
+pacman --needed --noconfirm -Syyuu
+
+packages=$(pacman -Qq)
+pacman --noconfirm -Runs -- ${packages//$'\npacman\n'/$'\n'}
+pacman --needed --noconfirm -Sy bash
