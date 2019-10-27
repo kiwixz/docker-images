c15988f2fc67515b522ae04631794a4e1d2c9c0a
kiwixz <kiwixz@outlook.com>
2019-10-27 16:40:47 +0100

    [arch] use official archlinux image to generate rootfs from docker

--- arch/make_in_docker.sh
+++ arch/make_in_docker.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker run -i --privileged --rm -v "$(pwd)/rootfs:/root/rootfs:ro" "kiwixz/arch" "/bin/bash" "-c"  \
+docker run -i --privileged --rm -v "$(pwd)/rootfs:/root/rootfs:ro" "archlinux" "/bin/bash" "-c"  \
     "pacman --needed --noconfirm -Sy arch-install-scripts tar >&2  \
         && /bin/bash >&2  \
         && cat rootfs.tar" <"make.sh" >"rootfs.tar"
